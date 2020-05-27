1215753c23e48c7b3e214991fd33685b9b04afea
lijiajia <lijiajia_2019@163.com>
2020-05-27 15:21:37 +0800

    在idea测试提交代码

---
+++ src/SomeTing/Test.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2019-2020, XXX有限公司
+ * FileName: Test
+ * Author:   李佳佳同学
+ * Date:     2020/5/27 15:11
+ * Description: 随便写写
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package SomeTing;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈随便写写〉
+ *
+ * @author 李佳佳同学
+ * @create 2020/5/27
+ * @since 1.0.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("故人是谁？" +
+                "姑苏林黛玉");
+    }
+}
